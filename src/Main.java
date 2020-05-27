15c094f983e943fd7fdbc6f3a51840ebef061767
kenta <khorie@sfu.ca>
2020-05-26 20:01:06 -0700

    just reviewing basics

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
-    public static void main(){
-        System.out.println("initial test. yes i know its late.");
+    public static void main(String args[]) {
+        int test = 0;
+        for (int count = 3; count != 0; count--) {
+            if (test == 0) {
+                System.out.println("initial test. yes i know its late.");
+            } else {
+                System.out.println("just testing if statements");
+            }
 
+            test++;
+        }
+        System.out.println("test="+test);
     }
 
 }
