d6b644146a3aa7119e6e2be2e6ee38e265f797e9
kenta <khorie@sfu.ca>
2020-05-26 19:50:42 -0700

    initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(){
-        System.out.println("initial test. yes i know its late");
+        System.out.println("initial test. yes i know its late.");
 
     }
 
